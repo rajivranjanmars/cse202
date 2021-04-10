3f570e49e641807a06058dee2d35dfef42b9470b
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-10 17:01:23 +0530

    size of various data type

---
+++ dataType.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    cout <<"size of bool is " << sizeof(bool) << endl;
+    cout << "size of char is " << sizeof(char) << endl;
+    cout << "size of int is " << sizeof(int) << endl;
+    cout << "size of short is " << sizeof(short) << endl;
+    cout << "size of long is " << sizeof(long) << endl;
+    cout << "size of long long is " << sizeof(long long) << endl;
+    cout << "size of float is " << sizeof(float) << endl;
+    cout << "size of double is " << sizeof(double) << endl;
+    cout << "size of long double is " << sizeof(long double) << endl;
+
+
+ return 0;
+}
