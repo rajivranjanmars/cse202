77df65a2e2cd89160a8095b0f6404e1c3def594f
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-12 20:14:09 +0530

    Write a program to print the sum and product of all the number in the given array.

---
+++ greatestInArray.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    int temp = arr[0];
+    for (int i = 0; i < 5; i++)
+    {
+        if (temp < arr[i])
+            temp = arr[i];
+    }
+
+    cout << temp << " is the largest element in array" << endl;
+
+    return 0;
+}
