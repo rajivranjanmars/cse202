49abc466d68f8451a64eddcaf123fbe36a6abd10
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-13 23:21:25 +0530

    C++ Program to Concatenate Two Strings

---
+++ stringConcatennate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+using namespace std;
+int main(){
+string str ,ptr;
+cin >> str >> ptr;
+int j=0;
+str=str+ptr;
+cout << str;
+
+ return 0;
+}
