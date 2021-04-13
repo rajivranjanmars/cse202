8dbad42b557dc6f9245c06927a530ca56e8d049f
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-13 10:36:16 +0530

    Define a function that returns the product of two numbers entered by user.

---
+++ productByFunction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+using namespace std;
+int product (int a, int b){
+    return a*b;
+}
+
+int main(){
+     int a,b;
+     cin >> a >> b;
+      cout <<  product(a,b);        
+ return 0;
+}
