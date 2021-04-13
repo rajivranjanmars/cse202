a68a0b999166a8b422778f6d8a40ee56ab6c0abc
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-13 10:29:23 +0530

    Write a program to reverse a string.

---
+++ revString.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main(){
+
+string str;
+cin >> str;
+
+
+
+reverse(str.begin(), str.end()) ;
+cout << str;
+
+ return 0;
+}
