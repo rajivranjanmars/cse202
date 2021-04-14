e95cbbb3b6f260403a95e2589195c5e161aa3579
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-14 12:26:15 +0530

    Write a program in  C++ to find the maximum and minimum element in an array

---
+++ arrayMaxMin.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+            cin >> arr[i];
+    }
+    int max =arr[0];
+    int min =arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i]>max)
+        max = arr[i];
+        if (arr[i]<min)
+        min = arr[i];
+    }
+    
+cout<< "max " <<max <<   endl;
+cout << "min" << min << endl;
+
+
+ return 0;
+}
