a810593c3426eae99d420077d4b2239e9566f365
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-19 16:40:03 +0530

    Write a structure to store the roll no., name, age (between 11 to 14) and address of students (more than 10). Store the information of the students.       1 - Write a function to print the names of all the students having age 14.       2 - Write another function to print the names of all the students having even roll no.       3 - Write another function to display the details of the student whose roll no is given (i.e.                 roll no. entered by the user).

--- studentStructure.cpp
+++ studentStructure.cpp
@@ -1,61 +1,64 @@
-#include <iostream>
 #include <cstdio>
-#include <vector>
+#include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-struct Student{
-int age, rollnum;
-string name , adress ;
+struct Student
+{
+    int age, rollnum;
+    string name, adress;
 };
 
-int main(){
-Student student[3];
-int a;
+int main()
+{
+    Student student[3];
+    int a;
 
-    for(int i=0; i<3;){
+    for (int i = 0; i < 3;)
+    {
         cin >> a;
-        if (a>=11&& a<=14){
-        student[i].age =a;
-        cin >> student[i].rollnum;
-        cin >> student[i].name;
-        cin >> student[i].adress;
-         i++;
+        if (a >= 11 && a <= 14)
+        {
+            student[i].age = a;
+            cin >> student[i].rollnum;
+            cin >> student[i].name;
+            cin >> student[i].adress;
+            i++;
         }
-        else{
+        else
+        {
             cout << "put a number between 10 and 14" << endl;
         }
     }
     cout << "Student with age 14" << endl;
-    for (int i = 0; i < 3; i++){
-        if (student[i].age==14)
+    for (int i = 0; i < 3; i++)
+    {
+        if (student[i].age == 14)
         {
-           cout << student[i].name << endl;/* code */
+            cout << student[i].name << endl; /* code */
         }
     }
     cout << "Student with even roll number " << endl;
-        for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
+    {
+        if (student[i].rollnum % 2 == 0)
         {
-         if (student[i].rollnum %2==0)
-         {
-             /* code */
-             cout <<student[i].name << endl;
-         }
             /* code */
+            cout << student[i].name << endl;
         }
-        int t;
-        cin >> t;
-        for (int i = 0; i < 3;i++){
-            if (student[i].rollnum==t)
-            {
-                /* code */
-                cout << student[i].name << " " << student[i].age << " " << student[i].adress << endl;
-            }
-            
+        /* code */
+    }
+    int t;
+    cin >> t;
+    for (int i = 0; i < 3; i++)
+    {
+        if (student[i].rollnum == t)
+        {
+            /* code */
+            cout << student[i].name << " " << student[i].age << " " << student[i].adress << endl;
         }
-        
-    
-    
+    }
 
- return 0;
-}                                                            
+    return 0;
+}
