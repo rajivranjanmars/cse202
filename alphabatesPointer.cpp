4a2500802e6e000f00661b8881102514d8c2b9ac
rajivranjanmars <rajivranjanmars@gmail.com>
2021-04-22 11:31:21 +0530

    Write a program  to print all the alphabets using a pointer.

--- alphabatesPointer.cpp
+++ alphabatesPointer.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 
 using namespace std;
-int main(){
-    char a=65;
-    char *c=&a;
-    while (   *c<=90 )
+int main()
+{
+    char a = 65;
+    char *c = &a;
+    while (*c <= 90)
     {
-        cout  << *c<< " " ;
-        *c=*c+1;
+        cout << *c << " ";
+        *c = *c + 1;
     }
     cout << endl;
-    *c=*c+6;
-    while (  *c<=122 )
+    *c = *c + 6;
+    while (*c <= 122)
     {
-        cout  << *c<< " " ;
-        *c=*c+1;
+        cout << *c << " ";
+        *c = *c + 1;
     }
 
-
-
- return 0;
+    return 0;
 }
