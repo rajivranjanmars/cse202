7061a8e7be444d597d4afbdf287330ed483d1fd5
Rajiv Ranjan <rajivranjanmars@gmail.com>
2021-09-08 02:10:09 +0530

    s

---
+++ s.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+using namespace std;
+int main(){
+                 
+   ios_base::sync_with_stdio(false);
+cin.tie(NULL);               
+              
+       cout<< "  saumya sri pagal hai"<< endl;
+       string s="iss las dimag nai hai"       ;
+
+        if (s.length()>4)
+          {
+             cout << s << endl;           /* code */
+        }
+        cout <<endl << endl << endl <<"or sunegi"<< endl<<endl; 
+        int t =4;
+        while (t--){
+                    
+       cout<< "  saumya sri pagal hai"<< endl;
+         cout << s << endl;
+        }
+}
